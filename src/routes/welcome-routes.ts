aca3a15057e84c0820daebb98ec2aa1f9427999f
Jonatan Lima <jotanlima@gmail.com>
2021-09-25 15:29:38 -0300

    feat: add configs to debug

--- src/routes/welcome-routes.ts
+++ src/routes/welcome-routes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/welcome', (req: Request, res: Response) => {
+    debugger
     res
       .status(200)
       .json({ message: 'Welcome to Balu bal' })
