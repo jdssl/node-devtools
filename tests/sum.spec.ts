5abd30d21152973402396c3cd2420fa3a65f6f82
Jonatan Lima <jotanlima@gmail.com>
2022-04-27 09:17:42 -0300

    feat: improve tests

--- tests/sum.spec.ts
+++ tests/sum.spec.ts
@@ -1,13 +1,12 @@
 describe('Sum', () => {
 	it('should return a correct sum value', () => {
-		const test = 'Test testadad'
-		const as = 'test dsssssd'
-
 		expect(1 + 1).toBe(2)
 		expect(2 + 2).toBe(4)
 	})
 	it('test', () => {
-		const messa = "test"
+		const message = "test"
 		debugger
+
+    expect(message).toEqual("test")
 	})
 })
