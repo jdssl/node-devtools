ea32c65627212eacc24356d2d62c4df50a0a4b95
Jonatan Lima <jotanlima@gmail.com>
2022-04-27 09:21:44 -0300

    refactor: lint

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -5,7 +5,9 @@ import { join } from 'path'
 export default (app: Express): void => {
   const router = Router()
   const prefix = '/api'
+
   app.use(prefix, router)
+
   const routesPath = join(__dirname, '/../routes')
 
   readdirSync(routesPath).map(async file => {
