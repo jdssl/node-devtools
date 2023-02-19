4543ed1a450ec00b077d0549b4d6ed0c955c8c93
Jonatan Lima (balu) <jotanlima@gmail.com>
2023-02-19 09:20:23 -0300

    add simple graceful shudtown

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import env from "./config/env";
 
 const { PORT } = env;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+(async () => {
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+})().catch((err) => {
+    console.log(`Unexpected error on run the project: ${err}`);
+    process.abort();
 });
