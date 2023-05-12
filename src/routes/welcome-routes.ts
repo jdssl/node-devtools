8b4a139b1cd44040e59c74c3b09749412803b332
balu <jotanlima@gmail.com>
2023-05-12 15:30:47 -0300

    refactor: improve message

--- src/routes/welcome-routes.ts
+++ src/routes/welcome-routes.ts
@@ -2,6 +2,6 @@ import { Request, Response, Router } from "express";
 
 export default (router: Router): void => {
     router.get("/welcome", (req: Request, res: Response) => {
-        res.status(200).json({ message: "Welcome to Balu" });
+        res.status(200).json({ message: "Welcome" });
     });
 };
